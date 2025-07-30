b042c88953630bd8b9290d7ee1d57d658afa5f5f
adriantech-beep <Adhrianne29@gmail.com>
2025-07-30 15:04:05 +0800

    fixed .env variables

--- src/services/apiProducts.js
+++ src/services/apiProducts.js
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
   try {
-    const res = await fetch(import.meta.env.VITE_API_URL);
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/products`);
     const data = await res.json();
     return data.products || [];
   } catch (err) {
